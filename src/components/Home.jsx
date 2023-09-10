f97600af83b5dc1551b04e7c20300ca5b227e15a
Abhishek-ai-ml <abhishekdoulia@gmail.com>
2023-09-10 14:31:25 +0530

    home component changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = ({ data }) => {
+    const [knowMore, setKnowMore] = useState(false);
     console.log("The data is ====", data);
   return (
-    <div className="text-white px-20 py-5 bg-slate-600 h-screen">
-      <div className="h-200 w-30 flex ">
-        <div className="flex-col">
-          <div className="text-start font-bold text-3xl mb-5">
-            Photo of the day
-          </div>
-          <div className="h-[400px] w-30 shadow-none hover:shadow-lg cursor-pointer">
-            <img
-              src={data.hdurl}
-              alt="todaypicture"
-              className="h-[400px] w-30 rounded-lg"
-            />
-          </div>
-          <div className="text-base mt-3 ">Photographer:{data.copyright}</div>
-          <div className="text-base mt-1 ">Date:{data.date}</div>
-        </div>
-
-        <div className="mt-12 ml-10">
-          <div className="text-sm max-w-lg"><b>Discription:</b>{data.explanation}</div>
-        </div>
-      </div>
+    <div className="w-full h-screen relative">
+        <p className="absolute left-10 top-5 border-l-2 border-t-2 p-3 pr-7 rounded-lg text-white text-3xl font-semibold">Astronomical Picture Of The Day</p>
+        <h1 className="absolute left-10 bottom-16 border-l-2 border-b-2 p-3 pr-7 rounded-lg text-white text-3xl font-semibold">{data.title}</h1>
+        <div className={`absolute text-white right-5 bottom-20 bg-transparent w-[800px] ${knowMore ? "p-5 bg-white rounded lg text-black w-[500px] shadow-[0_3px_10px_rgb(0,0,0,0.2)]" : ""} transition-all duration-200 ease-in-out`}>{knowMore ? data.explanation : data.explanation?.substr(0, 99)}... <button className="underline" onClick={() =>setKnowMore(!knowMore)}>{knowMore ? "Show Less" : "Know More"}</button></div>
+        <img src={data.hdurl} className="w-full h-screen"/>
     </div>
   );
 };
