e2f691abef894833258e4f635618e510b8978897
Abhishek-ai-ml <abhishekdoulia@gmail.com>
2023-10-08 16:49:50 +0530

    changes in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = ({ data }) => {
     <div className="w-full h-screen relative">
         <p className="absolute left-10 top-5 border-l-2 border-t-2 p-3 lg:pr-7 rounded-lg text-white lg:text-3xl text-xl font-semibold">Astronomical Picture Of The Day</p>
         <h1 className="absolute left-10 bottom-16 border-l-2 border-b-2 p-3 pr-7 rounded-lg text-white text-3xl font-semibold hidden lg:inline">{data.title}</h1>
-        <div className={`absolute lg:right-5 right-0 bottom-20 bg-transparent ${knowMore ? "text-black" : "text-white"} ${knowMore ? "lg:p-8 p-3 rounded-lg bg-white text-justify font-semibold z-10 lg:w-[600px] w-[300px] lg:h-fit h-[300px] shadow-[0_3px_10px_rgb(0,0,0,0.2)]" : ""} transition-all duration-200 ease-in-out overflow-y-scroll`}>{knowMore ? data.explanation : data.explanation?.substr(0, 80)}... <button className="underline" onClick={() =>setKnowMore(!knowMore)}>{knowMore ? "Show Less" : "Know More"}</button></div>
+        <div className={`absolute lg:right-5 right-0 bottom-20 bg-transparent ${knowMore ? "text-black" : "text-white"} ${knowMore ? "lg:p-8 p-3 rounded-lg bg-white text-justify font-semibold z-10 lg:w-[600px] w-[270px] lg:h-fit h-[300px] shadow-[0_3px_10px_rgb(0,0,0,0.2)]" : ""} transition-all duration-200 ease-in-out overflow-y-scroll`}>{knowMore ? data.explanation : data.explanation?.substr(0, 80)}... <button className="underline" onClick={() =>setKnowMore(!knowMore)}>{knowMore ? "Show Less" : "Know More"}</button></div>
         <img src={data.hdurl} className="w-full h-screen"/>
     </div>
   );
